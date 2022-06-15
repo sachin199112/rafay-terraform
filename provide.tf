dc25fb9bc4fcf87865608ef1bded08791c2c48be
sachin199112 <107113435+sachin199112@users.noreply.github.com>
2022-06-15 09:31:16 +0530

    Delete provide.tf

--- provide.tf
+++
@@ -1,6 +0,0 @@
-backend "s3" {
-    bucket         = "sachin-terraform"
-    key            = "terraform/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "terraform-sachin"
-}
